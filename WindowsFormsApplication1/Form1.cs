fd58399e66826c58d86d569a7bae10d7f91426d7
liuranwang <117189174@qq.com>
2020-12-04 08:39:48 +0800

    ??

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -11,7 +11,8 @@ using System.Windows.Forms;
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
-    {
+    {    
+
         public Form1()
         {
             InitializeComponent();
